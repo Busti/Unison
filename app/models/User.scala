5acc12047006fef64b08f25282e58393acefc776
Busti <mbust@live.de>
2017-02-02 01:30:08 +0100

    Started working on database models.

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,3 +1,31 @@
 package models
 
-class User(id: Long, username: String, password: String, salt: String)
+import play.api.Play
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.H2Driver
+import slick.driver.H2Driver.api._
+import slick.lifted.ProvenShape
+
+import scala.concurrent.Future
+
+case class User(id: Long, username: String, passHash: String, passSalt: String)
+
+class UserTableDef(tag: Tag) extends Table[User](tag, "user") {
+  def id       = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def username = column[String]("username")
+  def passHash = column[String]("password")
+  def passSalt = column[String]("salt")
+
+  override def * : ProvenShape[User] =
+    (id, username, passHash, passSalt) <>(User.tupled, User.unapply)
+}
+
+object Users {
+  val dbConfig = DatabaseConfigProvider.get[H2Driver](Play.current)
+
+  val users = TableQuery[UserTableDef]
+
+  def add(user: User): Future[String] = {
+    dbConfig.db.run(users += user).map(res =>)
+  }
+}
