8f8d33c00c8ed3172023d0edf96dbadd4cb2084a
busti <mbust@live.de>
2017-02-24 01:30:16 +0100

    Finished user-model find method.

--- app/models/UserDAO.scala
+++ app/models/UserDAO.scala
@@ -3,7 +3,10 @@ package models
 import com.google.inject.Inject
 import com.mohiva.play.silhouette.api.LoginInfo
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import slick.driver.H2Driver
+
+import scala.concurrent.Future
 
 class UserDAO @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) extends UserTableDef with HasDatabaseConfigProvider[H2Driver] {
   import driver.api._
@@ -11,7 +14,7 @@ class UserDAO @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)
   def loginInfoQuery(loginInfo: LoginInfo) =
     loginInfos.filter(dbLoginInfo => dbLoginInfo.providerID === loginInfo.providerID && dbLoginInfo.providerKey === loginInfo.providerKey)
 
-  def find(loginInfo: LoginInfo) = {
+  def find(loginInfo: LoginInfo): Future[Option[User]] = {
     val query = for {
       queryLoginInfo <- loginInfoQuery(loginInfo)
       queryUserLoginInfo <- userLoginInfos.filter(_.loginInfoId === queryLoginInfo.id)
@@ -20,7 +23,7 @@ class UserDAO @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)
 
     db.run(query.result.headOption).map { dbUserOption =>
       dbUserOption.map { user =>
-        return user
+        user
       }
     }
   }
