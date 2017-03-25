6880bbd34e4ad5bc640247b122767faf112d4592
Busti <mbust@live.de>
2017-03-25 16:13:02 +0100

    Finished user DAO. This took some time, as it turns out it was a misplaced bracket.

--- app/models/UserDAO.scala
+++ app/models/UserDAO.scala
@@ -9,8 +9,6 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import slick.dbio.DBIOAction
 import slick.driver.H2Driver
 
-import scala.concurrent.Future
-
 
 class UserDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends UserTableDef with HasDatabaseConfigProvider[H2Driver] {
 
@@ -25,11 +23,8 @@ class UserDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) 
     * @param loginInfo The Login Info of the User.
     * @return A Future Option populated with the user if found.
     */
-  def find(loginInfo: LoginInfo): Future[Option[User]] = {
-    /* Create a query that first retrieves a LoginInfo to get it's ID
-     * It then retrieves the User LoginInfo relation by the LoginInfo's
-     * id from the userLoginInfos table from which it uses the specified
-     * uuid of the User to retrieve an instance of the DBUser.
+  def find(loginInfo: LoginInfo) = {
+    /* Comprehension that creates a query doing the following
      */
     val query = for {
       queryLoginInfo <- loginInfoQuery(loginInfo)
@@ -37,7 +32,7 @@ class UserDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) 
       queryUser <- users.filter(_.uuid === queryUserLoginInfo.userId)
     } yield queryUser
 
-    //Exectutes the query and stores the resulting DBUser and the initial LoginInfo into a User
+    //Executes the query and stores the resulting DBUser and the initial LoginInfo into a User
     db.run(query.result.headOption).map { dbUserOption =>
       dbUserOption.map { user =>
         new User(loginInfo, user.uuid, user.username, user.email, user.avatarURL)
@@ -51,7 +46,7 @@ class UserDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) 
     * @param id The UUID of the desired User.
     * @return A Future Option populated with the user if found.
     */
-  def find(id: UUID): Future[Option[User]] = {
+  def find(id: UUID) = {
     val query = for {
       queryUser <- users.filter(_.uuid === id)
       queryUserLoginInfo <- userLoginInfos.filter(_.userId === queryUser.uuid)
@@ -67,65 +62,39 @@ class UserDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) 
           )
       }
     }
-
-    def save(user: User) = {
-      val dbUser = DBUser(user.uuid, user.username, user.email, user.avatarURL)
-      val dBLoginInfo = DBLoginInfo(None, user.loginInfo.providerID, user.loginInfo.providerKey)
-
-      val loginInfoAction = {
-        val retrieveLoginInfo = loginInfos.filter(
-          info =>
-            info.providerID === user.loginInfo.providerID
-            &&
-            info.providerKey === user.loginInfo.providerKey
-        ).result.headOption
-
-        val insertLoginInfo = loginInfos.returning(loginInfos.map(_.id)).into(
-          (info, id) =>
-            info.copy(id = Some(id))
-        ) += dBLoginInfo
-
-        for {
-          loginInfoOption <- retrieveLoginInfo
-          loginInfo <- loginInfoOption.map(DBIO.successful(_)).getOrElse(insertLoginInfo)
-        } yield loginInfo
-
-        val actions = (for {
-          _ <- users.insertOrUpdate(dbUser)
-          loginInfo <- loginInfoAction
-          _ <- userLoginInfos += DBUserLoginInfo(dbUser.uuid, loginInfo.id.get)
-        } yield ()).transactionally
-
-        db.run(actions).map(_ => user)
-      }
-    }
   }
 
-  /*
   def save(user: User) = {
     val dbUser = DBUser(user.uuid, user.username, user.email, user.avatarURL)
     val dBLoginInfo = DBLoginInfo(None, user.loginInfo.providerID, user.loginInfo.providerKey)
 
     val loginInfoAction = {
-      val getLoginInfo = loginInfos.filter(info =>
-        info.providerID === user.loginInfo.providerID
-          &&
-          info.providerKey === user.loginInfo.providerKey
+      val retrieveLoginInfo = loginInfos.filter(
+        info =>
+          info.providerID === user.loginInfo.providerID
+            &&
+            info.providerKey === user.loginInfo.providerKey
       ).result.headOption
 
-      val setLoginInfo = loginInfos.returning(
-        loginInfos.map(_.id)).into((info, id) =>
-        info.copy(id = Some(id))
+      val insertLoginInfo = loginInfos.returning(loginInfos.map(_.id)).into(
+        (info, id) =>
+          info.copy(id = Some(id))
       ) += dBLoginInfo
 
-      val actions = (for {
+      for {
+        loginInfoOption <- retrieveLoginInfo
+        loginInfo <- loginInfoOption.map(DBIO.successful).getOrElse(insertLoginInfo)
+      } yield loginInfo
+    }
+
+    val actions = (
+      for {
         _ <- users.insertOrUpdate(dbUser)
         loginInfo <- loginInfoAction
-        _ <- slickUserLoginInfos += DBUserLoginInfo(dbUser.uuid, loginInfo.id)
-      } yield ()).transactionally
+        _ <- userLoginInfos += DBUserLoginInfo(dbUser.uuid, loginInfo.id.get)
+      } yield ()
+      ).transactionally
 
-      db.run(actions).map(_ => user)
-    }
+    db.run(actions).map(_ => user)
   }
-  // */
 }
