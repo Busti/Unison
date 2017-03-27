511caea69cf4bf6779ab826a174965609936cae5
Busti <mbust@live.de>
2017-03-28 00:42:02 +0200

    Switched from regular bootstrap to sass bootstrap and integrated it with the missing Webjars injection. Unfortunately it seems like this injection is needed for the locate method to work. It adds a bunch of bloat but makes adding webjars easier.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,9 +5,8 @@ import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+class Application @Inject()(webJarAssets: WebJarAssets, val messagesApi: MessagesApi) extends Controller with I18nSupport {
   def index = Action { implicit request =>
-    
-    Ok(views.html.index())
+    Ok(views.html.index(webJarAssets))
   }
 }
