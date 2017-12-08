d5497201a5adbafd8ac0c2642926e9ff4fc588c1
Busti <mbust@live.de>
2017-12-08 22:30:09 +0100

    Implemented PasswordInfoDAO for slick.

--- app/models/daos/slick/PasswordInfoDAOImplSlick.scala
+++ app/models/daos/slick/PasswordInfoDAOImplSlick.scala
@@ -8,7 +8,7 @@ import com.mohiva.play.silhouette.persistence.daos.DelegableAuthInfoDAO
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.H2Profile
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class PasswordInfoDAOImplSlick @Inject()
 (
@@ -63,9 +63,28 @@ class PasswordInfoDAOImplSlick @Inject()
       map(dbPasswordInfo => (dbPasswordInfo.hasher, dbPasswordInfo.hash, dbPasswordInfo.salt)).
       update((passwordInfo.hasher, passwordInfo.password, passwordInfo.salt))
 
-  override def find(loginInfo: LoginInfo) = ???
-  override def add(loginInfo: LoginInfo, authInfo: PasswordInfo) = ???
-  override def update(loginInfo: LoginInfo, authInfo: PasswordInfo) = ???
-  override def save(loginInfo: LoginInfo, authInfo: PasswordInfo) = ???
-  override def remove(loginInfo: LoginInfo) = ???
+  def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = {
+    db.run(passwordInfoQuery(loginInfo).result.headOption).map { dbPasswordInfoOption =>
+      dbPasswordInfoOption.map(dbPasswordInfo =>
+        PasswordInfo(dbPasswordInfo.hasher, dbPasswordInfo.hash, dbPasswordInfo.salt))
+    }
+  }
+
+  def add(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] =
+    db.run(addAction(loginInfo, authInfo)).map(_ => authInfo)
+
+  def update(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] =
+    db.run(updateAction(loginInfo, authInfo)).map(_ => authInfo)
+
+  def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
+    val query = loginInfoQuery(loginInfo).joinLeft(passwordInfos).on(_.id === _.loginInfoId)
+    val action = query.result.head.flatMap {
+      case (dbLoginInfo, Some(dbPasswordInfo)) => updateAction(loginInfo, authInfo)
+      case (dbLoginInfo, None) => addAction(loginInfo, authInfo)
+    }
+    db.run(action).map(_ => authInfo)
+  }
+
+  def remove(loginInfo: LoginInfo): Future[Unit] =
+    db.run(passwordInfoSubQuery(loginInfo).delete).map(_ => ())
 }
